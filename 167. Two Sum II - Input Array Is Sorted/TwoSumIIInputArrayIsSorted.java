e474f48a665a35fe1c5d39a222a064b976c9b2d2
AndrijaLazic <lazic.andrija223@gmail.com>
2025-01-27 11:29:46 +0100

    Added leetcode solutions for task 167 optimal

--- 167. Two Sum II - Input Array Is Sorted/TwoSumIIInputArrayIsSorted.java
+++ 167. Two Sum II - Input Array Is Sorted/TwoSumIIInputArrayIsSorted.java
@@ -15,13 +15,51 @@ public class TwoSumIIInputArrayIsSorted {
         return null;
     }
 
+    public int[] twoSumOptimal(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left <= right){
+            int value = numbers[left] + numbers[right];
+            if(value > target){
+                right = Arrays.binarySearch(numbers , target - numbers[left]);
+                if(right < 0)
+                    right = -right - 2;
+            } else if (value < target) {
+                left = Arrays.binarySearch(numbers , target - numbers[right]);
+                if(left < 0)
+                    left = -left - 1;
+            }else {
+                if(left == right){
+                    //this is only because of possibility of duplicates
+                    if (left - 1 >= 0 && numbers[left - 1] == numbers[left]){
+                        left--;
+                    }else {
+                        right++;
+                    }
+                }
+
+                return new int[]{left + 1, right + 1};
+            }
+        }
+
+        return null;
+    }
+
+//    twoPointersSolution{
+//        int l = 0, r = nums.length - 1;
+//
+//        while (nums[l] + nums[r] != target) {
+//            if (nums[l] + nums[r] < target) l++;
+//            else r--;
+//        }
+//
+//        return new int[] {l+1, r+1};
+//    }
+
     public static void main(String[] args) {
         TwoSumIIInputArrayIsSorted t = new TwoSumIIInputArrayIsSorted();
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(t.twoSum(nums, target)));
-
-
-
+        int[] nums = {-1,-1,1,1,1,1,1,1,1,1,1};
+        int target = -2;
+        System.out.println(Arrays.toString(t.twoSumOptimal(nums, target)));
     }
 }
